c7b2ed9ef77b5de1581f0a1b1e1865ff0379d7f3
Santhosha01 <159224526+Santhosha01@users.noreply.github.com>
2024-11-12 16:36:14 +0530

    Delete BankManagementApplication directory

--- BankManagementApplication/src/main/java/com/bankapplication/repo/AccountRepo.java
+++
@@ -1,9 +0,0 @@
-package com.bankapplication.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bankapplication.entity.Account;
-
-public interface AccountRepo extends JpaRepository<Account, Long> {
-
-}
